809020d41a98bffc85e19fb01ec03b089cc1c449
brroge <168661567+brroge@users.noreply.github.com>
2025-11-01 20:57:39 -0500

    Update decade_old.py

--- decade_old.py
+++ decade_old.py
@@ -1,5 +1,10 @@
+# Ask the question
 age = int(input("HOW OLD ARE YOU?\n"))
 
+# The function to calculate decades and years
 decades = age //10
+years = age % 10
 
-print("You are " + str(decades) + " decades old.")
+# Print the result
+print("You are " + str(decades) + 
+      " decades and " + str(years) + " years old.")
